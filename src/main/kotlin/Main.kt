0b3e87823d481685db2cc8b4272baf8e3053d5e3
Andrew Tieszen <pilottzn@gmail.com>
2023-10-21 21:21:41 -0600

    Check if string entered is a palindrome

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("-----even or odd-----")
+    evenOdd()
+    println("-----palindrome------")
+    isPalindrome()
+}
+
+fun evenOdd() {
+    val s = Scanner(System.`in`)
+    println("Please enter an integer: ")
+    val number = s.nextInt()
+
+    val result = if(number % 2 == 0) {
+        "even"
+    } else {
+        "odd"
+    }
+
+    println("$number is $result.")
+}
+
+fun isPalindrome() {
+    val s = Scanner(System.`in`)
+    println("Please enter a string: ")
+    val string1 = s.next()
+
+    val forward = string1.toCharArray()
+    val reversed = forward.reversed()
+    val string2 = reversed.joinToString("")
+
+    val result = if(string1 == string2) "" else "not "
+    println("$string1 is $result" + "a palindrome.")
 }
